3be4e6c3d1363b5d638ef3fa44de5a05da1c4057
Huang-ChingHsuan <oliver8410252594@gmail.com>
2021-06-20 14:19:25 +0800

    seventh commit hw1 fixed

--- homeworks/week11/hw1/delete_comment.php
+++ homeworks/week11/hw1/delete_comment.php
@@ -13,12 +13,19 @@
         header('Location: ./index.php?errorCode=1');
         die($conn->error);
     }
-    
-    $sql = "UPDATE chinghsuan_board_comments SET is_deleted=1 WHERE id=? AND username=?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param('is', $id, $username);
-    $result = $stmt->execute();
 
+    // 分為管理者與一般使用者
+    if($username && $authority === 'admin'){
+        $sql = "UPDATE chinghsuan_board_comments SET is_deleted=1 WHERE id=?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('i', $id);
+    } else {
+        $sql = "UPDATE chinghsuan_board_comments SET is_deleted=1 WHERE id=? AND username=?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('is', $id, $username);
+    }
+    
+    $result = $stmt->execute();
 
     if(!$result) {
         die('Error: ' . $conn->error);
