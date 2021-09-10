585549ba822a8ec335e95cedb94c95153ff67ccf
Hsuan <oliver8410252594@gmail.com>
2021-09-10 20:31:47 +0800

    Delete homeworks/week22 directory

--- homeworks/week22/hw1/my-blog/src/utils.js
+++
@@ -1,9 +0,0 @@
-const TOKEN_NAME = "token";
-
-export const setAuthToken = (token) => {
-  localStorage.setItem(TOKEN_NAME, token);
-};
-
-export const getAuthToken = () => {
-  return localStorage.getItem(TOKEN_NAME);
-};
